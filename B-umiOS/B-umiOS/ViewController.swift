dc6663399a08a5a60c8c8ab6f5c9facad22cfa8a
inaeLee <inae9709@hanmail.net>
2021-06-29 01:24:43 +0900

    init project

---
+++ B-umiOS/B-umiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  B-umiOS
+//
+//  Created by inae Lee on 2021/06/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
