09738ebb074eb7e879e9c8eb9ec577a95b0b7f51
inaeLee <inae9709@hanmail.net>
2021-07-01 02:00:07 +0900

    add: Home Dir 추가

---
+++ B-umiOS/B-umiOS/Home/Cell/CardCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CardCollectionViewCell.swift
+//  B-umiOS
+//
+//  Created by inae Lee on 2021/07/01.
+//
+
+import UIKit
+
+class CardCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ B-umiOS/B-umiOS/Home/ViewController/HomeViewController.swift
@@ -0,0 +1,28 @@
+//
+//  HomeViewController.swift
+//  B-umiOS
+//
+//  Created by inae Lee on 2021/07/01.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    // MARK: - UIComponenets
+    
+    // MARK: - Properties
+    
+    // MARK: - Initializer
+    
+    // MARK: - LifeCycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Actions
+    
+    // MARK: - Methods
+    
+    // MARK: - Protocols
+}
