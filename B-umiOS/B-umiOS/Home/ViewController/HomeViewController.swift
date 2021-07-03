3122b7734c6509ac0e880dc446c533797fb58ecc
inaeLee <inae9709@hanmail.net>
2021-07-04 02:13:05 +0900

    [add] HomeVC 레이아웃 구성 + WritingVC Push 연결

--- B-umiOS/B-umiOS/Home/ViewController/HomeViewController.swift
+++ B-umiOS/B-umiOS/Home/ViewController/HomeViewController.swift
@@ -11,11 +11,31 @@ import UIKit
 
 class HomeViewController: UIViewController {
     // MARK: - UIComponenets
+
+    private let dateLabel = UILabel().then {
+        $0.text = Date().dateToString(date: Date())
+        $0.font = UIFont.nanumSquareFont(type: .extraBold, size: 20)
+        $0.textColor = .white
+    }
     
-    private let label = UILabel().then {
-        $0.text = "Home"
-        $0.font = UIFont.nanumSquareFont(type: .extraBold, size: 25)
-        $0.textColor = UIColor.green2Main
+    private let guideLabel = UILabel().then {
+        $0.text = "휴지통을 클릭해\n스트레스를 비워보세요"
+        $0.lineSpacing(spacing: 13)
+        $0.numberOfLines = 2
+        $0.textAlignment = .center
+        $0.font = UIFont.nanumSquareFont(type: .extraBold, size: 26)
+        $0.textColor = UIColor.white
+    }
+    
+    private let arrowImage = UIImageView().then {
+        $0.image = UIImage(named: "bgElements")
+    }
+    
+    private lazy var trashBinButton = UIButton().then {
+        $0.addTarget(self, action: #selector(didTapTrashBinButton(_:)), for: .touchUpInside)
+        $0.backgroundColor = .green3
+        $0.setTitle("쓰레기통", for: .normal)
+        $0.tintColor = .white
     }
     
     // MARK: - Properties
@@ -27,19 +47,49 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setView()
         setConstraint()
     }
     
     // MARK: - Actions
     
+    @objc
+    func didTapTrashBinButton(_ sender: UIButton) {
+        navigationController?.pushViewController(WritingViewController(), animated: true)
+    }
+    
     // MARK: - Methods
     
     func setConstraint() {
-        view.addSubviews([label])
+        view.addSubviews([dateLabel, guideLabel, arrowImage, trashBinButton])
         
-        label.snp.makeConstraints { make in
-            make.centerX.centerY.equalToSuperview()
+        dateLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(self.view.safeAreaLayoutGuide).offset(13)
         }
+        
+        guideLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(dateLabel.snp.bottom).offset(40)
+        }
+        
+        arrowImage.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(guideLabel.snp.bottom).offset(44)
+            make.width.height.equalTo(75 * UIScreen.main.bounds.width / 375)
+        }
+        
+        trashBinButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(arrowImage.snp.bottom).offset(50)
+        }
+    }
+    
+    func setView() {
+        view.backgroundColor = UIColor.blue2Main
+        
+        navigationController?.isNavigationBarHidden = true
+        navigationController?.interactivePopGestureRecognizer?.delegate = nil
     }
     
     // MARK: - Protocols
---
+++ B-umiOS/B-umiOS/Writing/WritingViewController.swift
@@ -0,0 +1,34 @@
+//
+//  WritingViewController.swift
+//  B-umiOS
+//
+//  Created by inae Lee on 2021/07/04.
+//
+
+import UIKit
+
+class WritingViewController: UIViewController {
+    // MARK: - UIComponenets
+    
+    // MARK: - Properties
+    
+    // MARK: - Initializer
+    
+    // MARK: - LifeCycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setView()
+    }
+    
+    // MARK: - Actions
+    
+    // MARK: - Methods
+    
+    func setView() {
+        view.backgroundColor = .white
+    }
+    
+    // MARK: - Protocols
+}
