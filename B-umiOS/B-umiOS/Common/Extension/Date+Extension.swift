84022e87aff03166d1fa18c81c41f422a488c254
inaeLee <inae9709@hanmail.net>
2021-07-03 22:47:29 +0900

    [add] arrowAssets 및 Date(DateToString), Label(행간) Extension 추가

---
+++ B-umiOS/B-umiOS/Common/Extension/Date+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  Date+Extension.swift
+//  B-umiOS
+//
+//  Created by inae Lee on 2021/07/03.
+//
+
+import Foundation
---
+++ B-umiOS/B-umiOS/Common/Extension/UILabel+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UILabel+Extension.swift
+//  B-umiOS
+//
+//  Created by inae Lee on 2021/07/03.
+//
+
+import Foundation
