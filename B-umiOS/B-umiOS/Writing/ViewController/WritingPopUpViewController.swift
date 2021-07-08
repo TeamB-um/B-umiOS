3f18e5bf6e1c6b3a817d1977277d5d7c3cb269f5
inaeLee <inae9709@hanmail.net>
2021-07-08 18:14:19 +0900

    [add] popUp UIComponents 레이아웃 잡음

--- B-umiOS/B-umiOS/Writing/ViewController/WritingPopUpViewController.swift
+++ B-umiOS/B-umiOS/Writing/ViewController/WritingPopUpViewController.swift
@@ -25,19 +25,21 @@ class WritingPopUpViewController: UIViewController {
     }
     
     private let closeButton = UIButton().then {
-        $0.setImage(UIImage(named: "btnClose"), for: .normal)
+        $0.setImage(UIImage(named: "btnCloseBlack"), for: .normal)
     }
     
     private let guideLabel = UILabel().then {
         $0.text = "삭제 시 설정 기한 이후 영구 삭제되고,\n보관 시 분리수거함에 저장됩니다."
         $0.numberOfLines = 0
+        $0.lineSpacing(spacing: 7)
+        $0.textAlignment = .center
         $0.font = UIFont.nanumSquareFont(type: .regular, size: 14)
         $0.textColor = .paper3
     }
     
     private let deleteButton = UIButton().then {
         $0.setTitle("삭제", for: .normal)
-        $0.titleLabel?.textColor = .paper3
+        $0.setTitleColor(.paper3, for: .normal)
         $0.cornerRound(radius: 10)
         $0.layer.borderWidth = 1.0
         $0.layer.borderColor = UIColor(red: 210/255, green: 210/255, blue: 210/255, alpha: 1).cgColor
@@ -75,9 +77,39 @@ class WritingPopUpViewController: UIViewController {
         popUpView.addSubviews([closeButton, titleLabel, guideLabel, deleteButton, archiveButton])
         
         popUpView.snp.makeConstraints { make in
-            make.width.equalTo(375).multipliedBy(0.9)
-            make.height.equalTo(popUpView.snp.width).multipliedBy(0.5)
+            make.width.equalTo(343 * SizeConstants.ScreenRatio)
+            make.height.equalTo(226 * SizeConstants.ScreenRatio)
             make.center.equalToSuperview()
+        }
+        
+        closeButton.snp.makeConstraints { make in
+            make.width.height.equalTo(48 * SizeConstants.ScreenRatio)
+            make.top.equalToSuperview().inset(4 * SizeConstants.ScreenRatio)
+            make.trailing.equalToSuperview().inset(8 * SizeConstants.ScreenRatio)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(28 * SizeConstants.ScreenRatio)
+            make.centerX.equalToSuperview()
+        }
+        
+        guideLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(10 * SizeConstants.ScreenRatio)
+            make.centerX.equalToSuperview()
+        }
+        
+        deleteButton.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(24 * SizeConstants.ScreenRatio)
+            make.bottom.equalToSuperview().offset(-32 * SizeConstants.ScreenRatio)
+            make.width.equalTo(141 * SizeConstants.ScreenRatio)
+            make.height.equalTo(50 * SizeConstants.ScreenRatio)
+        }
+        
+        archiveButton.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().offset(-24 * SizeConstants.ScreenRatio)
+            make.bottom.equalToSuperview().offset(-32 * SizeConstants.ScreenRatio)
+            make.width.equalTo(141 * SizeConstants.ScreenRatio)
+            make.height.equalTo(50 * SizeConstants.ScreenRatio)
         }
     }
     
