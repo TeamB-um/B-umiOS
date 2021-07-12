4f6704511ae2e112224e7f0fee036ba07cf50e43
inaeLee <inae9709@hanmail.net>
2021-07-13 02:34:43 +0900

    [add] completion message 아이콘 이미지 추가

---
+++ B-umiOS/B-umiOS/ThrowTrash/Component/CompletionMessage.swift
@@ -0,0 +1,20 @@
+//
+//  CompletionMessage.swift
+//  B-umiOS
+//
+//  Created by inae Lee on 2021/07/13.
+//
+
+import UIKit
+
+class CompletionMessage: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
