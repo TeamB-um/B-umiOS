3831baec8dcd24915d99ccc11d0df8779009c51f
kong <1018dbrud@gmail.com>
2021-07-13 15:38:46 +0900

    [fix] 나의 글 상세 뷰 팝업 연결

--- B-umiOS/B-umiOS/MyPage/ViewController/MyWritingViewController.swift
+++ B-umiOS/B-umiOS/MyPage/ViewController/MyWritingViewController.swift
@@ -92,6 +92,16 @@ extension MyWritingViewController : UICollectionViewDataSource {
         let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: ButtonSectionView.identifier, for: indexPath)
         return headerView
     }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if !deleteButtonIsSelected {
+        let popUpVC = MyWritingPopUpViewController(writing: DummyWriting(trashBin: "날파리", title: "제목", date: Date(), content: "이 새벽을 비추는 초생달 오감보다 생생한 육감의 세계로 보내주는 푸르고 투명한 파랑새술취한 몸이 잠든 이 거릴 휘젓고 다니다 만나는 마지막 신호등이 뿜는 붉은 신호를 따라 회색 거리를 걸어서 가다보니 좀 낯설어 보이는 그녀가 보인적 없던 눈물로 나를 반겨 태양보다 뜨거워진 나 그녀의 가슴에 안겨 창가로 비친 초승달 침대가로 날아온 파랑새가 전해준그녀의 머리핀을 보고 눈물이 핑돌아 순간 픽하고 나가버린 시야는 오감의 정전을 의미 이미 희미해진 내 혼은 보라빛 눈을 가진 아름다운 그녀를 만나러 파랑새를 따라 몽환의 숲으로 나는 날아가 단 둘만의 가락에 오감의 나락에 아픔은 잊어버리게 내 손은 그녀의 치마자락에 하늘에 날린 아드레날린 하나도 화날일 없는 이곳은 그녀와 나 파랑새만이 육감의 교감으로 오감따위는 초월해버린 기적의 땅 얼만큼의 시간이 지났는지 몰라 허나 한숨자고 깨어봐도 여전히 니 품안이라는게 꼬집어봐도 꿈이 아니라는게 행복해 만족해 잠시보이는 무지개같은 사랑이라 해도 흩어질 잊혀질 구름이라 해도 터질듯해 내 감정은 머리로는 못해 이해를 스위치가 내려진 세상이 정신 건강의 도우미 그녈 마시고 취할거야 번지수는 몽환의 숲"))
+        
+        popUpVC.modalTransitionStyle = .crossDissolve
+        popUpVC.modalPresentationStyle = .overCurrentContext
+        self.present(popUpVC, animated: true, completion: nil)
+        }
+    }
 }
 
 extension MyWritingViewController : UICollectionViewDelegateFlowLayout {
