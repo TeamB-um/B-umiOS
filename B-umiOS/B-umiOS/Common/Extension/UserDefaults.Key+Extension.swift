83070369220b1c1d91180fbbd03d636d96da3982
inaeLee <inae9709@hanmail.net>
2021-07-13 21:27:08 +0900

    [add] UserDefaults.Keys 속성 분리

---
+++ B-umiOS/B-umiOS/Common/Extension/UserDefaults.Key+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UserDefaults.Key+Extension.swift
+//  B-umiOS
+//
+//  Created by inae Lee on 2021/07/13.
+//
+
+import UIKit
+
+extension UserDefaults {
+    enum Keys {
+        static let token = "token"
+    }
+}
