ced0a040c2e9789f4c1b21433967ae492603568a
inaeLee <inae9709@hanmail.net>
2021-07-13 13:04:22 +0900

    [add] splash lottie 파일 추가

---
+++ B-umiOS/B-umiOS/LaunchScreen/LaunchScreenViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LaunchScreenViewController.swift
+//  B-umiOS
+//
+//  Created by inae Lee on 2021/07/13.
+//
+
+import UIKit
+
+class LaunchScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
