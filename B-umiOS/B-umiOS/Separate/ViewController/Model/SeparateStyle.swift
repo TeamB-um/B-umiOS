699d32d68bd7b5c34ab4ba9611ba345bd4523959
Hong Jins Seok <sldkdl111@ajou.ac.kr>
2021-07-14 21:17:35 +0900

    [add] 분리수거함 스타일 모델 생성

---
+++ B-umiOS/B-umiOS/Separate/ViewController/Model/SeparateStyle.swift
@@ -0,0 +1,14 @@
+//
+//  SeparateColor.swift
+//  B-umiOS
+//
+//  Created by 홍진석 on 2021/07/14.
+//
+
+import Foundation
+import UIKit
+
+struct SeparateStyle {
+    static let color: [UIColor] = [.blue2Main, .green2Main, .pink3, .blue4, .green4, .green1, .blue3, .pink2Main]
+    static let image: [String] = ["seperate1_", "seperate2_","seperate3_", "seperate4_", "seperate5_", "seperate6_", "seperate7_", "seperate8_"]
+}
