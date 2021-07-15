10be7d8868d1beacffdd5a20cb1b272722907405
inaeLee <inae9709@hanmail.net>
2021-07-15 23:56:59 +0900

    [add] ActivityIndicator class 추가

--- B-umiOS/B-umiOS/Common/Indicator/ActivityIndicator.swift
+++ B-umiOS/B-umiOS/Common/Indicator/ActivityIndicator.swift
@@ -5,4 +5,32 @@
 //  Created by inae Lee on 2021/07/15.
 //
 
-import Foundation
+import Lottie
+import UIKit
+
+class ActivityIndicator {
+    static let shared = ActivityIndicator()
+    
+    let window = UIApplication.shared.windows.first
+    
+    private var lottieLoading = AnimationView().then {
+        $0.frame = CGRect(origin: .zero, size: CGSize(width: 80 * SizeConstants.screenRatio, height: 80 * SizeConstants.screenRatio))
+        $0.animation = Animation.named("loading")
+        $0.loopMode = .loop
+    }
+    
+    func startLoadingAnimation() {
+        window?.addSubview(lottieLoading)
+        
+        lottieLoading.snp.remakeConstraints { make in
+            make.center.equalToSuperview()
+        }
+        
+        lottieLoading.play()
+    }
+    
+    func stopLoadingAnimation() {
+        lottieLoading.stop()
+        lottieLoading.removeFromSuperview()
+    }
+}
