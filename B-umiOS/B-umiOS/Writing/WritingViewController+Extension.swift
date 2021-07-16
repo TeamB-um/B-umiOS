bfac71f7c3e8e21a6c89a4dfb86bc2ed97e9d6b3
inaeLee <inae9709@hanmail.net>
2021-07-16 18:51:24 +0900

    [fix] writingVC tag cell 그라데이션 버그 수정 (#113)

--- B-umiOS/B-umiOS/Writing/WritingViewController+Extension.swift
+++ B-umiOS/B-umiOS/Writing/WritingViewController+Extension.swift
@@ -56,12 +56,18 @@ extension WritingViewController: UICollectionViewDelegate {
     // FIXME: - 일단.. 잠시 사라지게만 해뒀음 좌표값에 따라 변경 필요
 
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        if scrollView.contentOffset.x < 10 {
+        if scrollView.contentOffset.x < 2 {
+            print(scrollView.contentOffset.x)
             UIView.animate(withDuration: 0.3) {
                 self.leftGradientView.alpha = 0
                 self.righrGradientView.alpha = 1
             }
-        } else if scrollView.contentOffset.x > scrollView.contentSize.width - scrollView.bounds.width {
+        } else if scrollView.contentOffset.x < scrollView.contentSize.width - scrollView.bounds.width {
+            UIView.animate(withDuration: 0.3) {
+                self.leftGradientView.alpha = 1
+                self.righrGradientView.alpha = 1
+            }
+        } else {
             UIView.animate(withDuration: 0.3) {
                 self.leftGradientView.alpha = 1
                 self.righrGradientView.alpha = 0
