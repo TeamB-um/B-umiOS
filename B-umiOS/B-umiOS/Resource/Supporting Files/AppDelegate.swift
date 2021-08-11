0bd246b89cfc458fcd87671b68f63beb0eae429e
inaeLee <inae9709@hanmail.net>
2021-08-11 23:05:11 +0900

    [add] firebase fcm 기본 설정 완료

--- B-umiOS/B-umiOS/Resource/Supporting Files/AppDelegate.swift
+++ B-umiOS/B-umiOS/Resource/Supporting Files/AppDelegate.swift
@@ -5,15 +5,28 @@
 //  Created by inae Lee on 2021/06/29.
 //
 
+import Firebase
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let notificationOptions = UNAuthorizationOptions(arrayLiteral: [.alert, .badge, .sound])
+
+        UNUserNotificationCenter.current().requestAuthorization(options: notificationOptions) { _, error in
+            if let error = error {
+                print(#function, error)
+            }
+        }
+
+        application.registerForRemoteNotifications()
+        FirebaseApp.configure()
+
+        UNUserNotificationCenter.current().delegate = self
+        Messaging.messaging().delegate = self
+
         return true
     }
 
@@ -22,7 +35,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
@@ -30,7 +43,57 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    /// foreground 상태일 때 push 알림 받음
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        let userInfo = notification.request.content.userInfo
+        print(userInfo, "👅")
+
+        /// 애널리틱스에 전달
+        Messaging.messaging().appDidReceiveMessage(userInfo)
+        completionHandler([.list, .badge, .sound, .banner])
+    }
+
+    /// background일 때
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        UIApplication.shared.applicationIconBadgeNumber += 1
+
+        let userInfo = response.notification.request.content.userInfo
+        print(userInfo, "👅")
+
+        Messaging.messaging().appDidReceiveMessage(userInfo)
+        completionHandler()
+    }
+}
+
+extension AppDelegate: MessagingDelegate {
+    func applicationDidFinishLaunching(_ application: UIApplication) {
+        print("applicationDidFinishLaunching")
+    }
+
+    /// 현재 등록 토큰 가져오기 (갱신 시 알림 받기)
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print( "🥺 [fcm token]: ", fcmToken)
+
+        /// 서버로 토큰 등록
+//        let dataDict: [String: String] = ["token": fcmToken ?? ""]
+//        NotificationCenter.default.post(
+//            name: Notification.Name("FCMToken"),
+//            object: nil,
+//            userInfo: dataDict
+//        )
+    }
+
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        print(userInfo, "💬")
+        Messaging.messaging().appDidReceiveMessage(userInfo)
+
+        completionHandler(UIBackgroundFetchResult.newData)
+    }
+
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
+    }
+}
