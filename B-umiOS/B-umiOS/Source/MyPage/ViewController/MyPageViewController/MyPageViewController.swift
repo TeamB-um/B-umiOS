8c6f93b7e53e8e2bd8c7968d02db1b2e2bc74524
kong <1018dbrud@gmail.com>
2021-08-21 02:36:07 +0900

    [fix] 보관함 뷰 reload data bug fix

--- B-umiOS/B-umiOS/Source/MyPage/ViewController/MyPageViewController/MyPageViewController.swift
+++ B-umiOS/B-umiOS/Source/MyPage/ViewController/MyPageViewController/MyPageViewController.swift
@@ -75,10 +75,19 @@ class MyPageViewController: UIViewController {
         setConstraints()
         setCollectionView()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        setCollectionViewData()
+    }
+
 
     // MARK: - Actions
 
     // MARK: - Methods
+    
+    func setCollectionViewData() {
+        menuSectionCollectionView.reloadData()
+    }
 
     func setCollectionView() {
         myPageMenuCollectionView.selectItem(at: IndexPath(row: 0, section: 0), animated: true, scrollPosition: .init())
