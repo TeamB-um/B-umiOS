9672a1932fdd233bc3fa433e81bae6bb5d86a14f
inaeLee <inae9709@hanmail.net>
2021-10-23 16:24:33 +0900

    [fix] 글쓰기 종이 비율 유지하도록 수정

--- B-umiOS/B-umiOS/Source/Writing/ViewController/WritingViewController/WritingViewController.swift
+++ B-umiOS/B-umiOS/Source/Writing/ViewController/WritingViewController/WritingViewController.swift
@@ -81,6 +81,7 @@ class WritingViewController: UIViewController {
     
     lazy var paperView = UIImageView().then {
         $0.image = self.style.paperIamge
+        $0.contentMode = .top
         $0.isUserInteractionEnabled = true
     }
     
